6b2b529f5b541536823348c1dc47d308ca6ddfbb
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2023-01-30 23:27:12 +0900

    update : mian script

--- dist/index.js
+++ dist/index.js
@@ -12,16 +12,22 @@ const startPack_1 = __importDefault(require("./startPack"));
 const options = (0, command_line_args_1.default)(args_1.default);
 const srcPath = (0, resolvePath_1.default)(options.src, options === null || options === void 0 ? void 0 : options.basedir);
 const outFilename = options.filename || path_1.default.basename(options.src);
-const distPath = (0, resolvePath_1.default)(path_1.default.dirname(options.dist), options === null || options === void 0 ? void 0 : options.baseDir);
+const distPath = (0, resolvePath_1.default)(path_1.default.dirname(options.dist), (options === null || options === void 0 ? void 0 : options.baseDir) || options.src);
 promises_1.default.access(srcPath, promises_1.default.constants.R_OK).then(run).catch(console.error);
 function run() {
-    console.log(`Started packing : ${srcPath}`);
-    console.log(distPath, outFilename);
-    (0, startPack_1.default)(srcPath).then(res => {
-        const zip = res[0];
-        const ext = res[1];
-        zip.generateAsync({ type: "nodebuffer" }).then(buffer => {
-            promises_1.default.writeFile(path_1.default.join(distPath, outFilename + ext), buffer, "binary");
-        });
-    });
+    try {
+        console.log(`Started packing : ${srcPath}`);
+        (0, startPack_1.default)(srcPath).then(res => {
+            const zip = res[0];
+            const ext = res[1];
+            zip.generateAsync({ type: "nodebuffer" }).then(buffer => {
+                promises_1.default.writeFile(path_1.default.join(distPath, outFilename + ext), buffer, "binary");
+            });
+            console.log(`Resources Packed : ${path_1.default.join(distPath, outFilename + ext)}`);
+        }).catch(console.error);
+    }
+    catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -9,20 +9,24 @@ const options = commandLineArgs(argsOption);
 
 const srcPath = resolvePath(options.src, options?.basedir);
 const outFilename = options.filename || path.basename(options.src);
-const distPath = resolvePath(path.dirname(options.dist),options?.baseDir);
+const distPath = resolvePath(path.dirname(options.dist), options?.baseDir || options.src);
 
 fs.access(srcPath, fs.constants.R_OK).then(run).catch(console.error)
 
 function run(){
-    console.log(`Started packing : ${srcPath}`);
-    console.log(distPath,outFilename);
-    
-    startPack(srcPath).then(res=>{
-        const zip = res[0];
-        const ext = res[1]
-        zip.generateAsync({type:"nodebuffer"}).then(buffer=>{
-            fs.writeFile(path.join(distPath,outFilename+ext),buffer,"binary");
-        })
-    })
+    try {
+        console.log(`Started packing : ${srcPath}`);
+        startPack(srcPath).then(res => {
+            const zip = res[0];
+            const ext = res[1]
+            zip.generateAsync({ type: "nodebuffer" }).then(buffer => {
+                fs.writeFile(path.join(distPath, outFilename + ext), buffer, "binary");
+            })
+            console.log(`Resources Packed : ${path.join(distPath, outFilename+ext)}`);
+        }).catch(console.error);
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
     
 }
